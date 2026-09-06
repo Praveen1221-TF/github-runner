10078c0c9803adf643c4b459bac876f060191273
Praveen1221-TF <praveensunnam123@gmail.com>
2026-09-06 15:12:19 +0530

    git-runner

---
+++ .github/workflows/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "practicedev.shop"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z0484880TU6HA0H1F3WH"
+  description = "description"
+}
